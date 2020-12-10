1f1cc06c0ebf5af810911eb23e5642ea2b6fbf9f
= <=>
2020-12-10 23:12:15 +0700

    update Web UI #1

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.querySelector(".browse-picture").addEventListener('click', () =>{
-	b = document.querySelector('.picture-name');
-	a = document.querySelector('.inline-picture', "#picture-1");
-	a.src = b.value;
-	b.value = "";
-});
+element = document.querySelectorAll(['button']);
+
+element.forEach((a) => {
+	a.addEventListener('mousemove', () => {
+	  a.style['background-color'] = '#9baeaf';
+	});
+	a.addEventListener('mouseleave', ()=>{
+	  a.style['background-color'] = '#ffffff';
+	});
+	a.addEventListener('click', ()=>{
+	  a.style['background-color'] = '#777777';
+	  a.style['font-style'] = 'italic';
+	  setTimeout(() =>{
+	    a.style['background-color'] = '#ffffff';
+	    a.style['font-style'] = 'normal';
+	  }, 200);
+	});
+})
+
